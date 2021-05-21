fa4ab7c4e44e3d4b90fb3832c83430e2557041a6
fac-73 <kaydavis21@gmail.com>
2021-05-20 22:54:15 -0700

    Updates

---
+++ precache-manifest.ca6e16b97ee989f8e77b446af6a73af7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8778000a63579089ce95e456c25ccfb0",
+    "url": "/substance/index.html"
+  },
+  {
+    "revision": "c578fb10b72426d74047",
+    "url": "/substance/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "339b626e6b7d1b2b7b20",
+    "url": "/substance/static/css/main.cb2c57dd.chunk.css"
+  },
+  {
+    "revision": "c578fb10b72426d74047",
+    "url": "/substance/static/js/2.5374e0e7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/substance/static/js/2.5374e0e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339b626e6b7d1b2b7b20",
+    "url": "/substance/static/js/main.341bd49a.chunk.js"
+  },
+  {
+    "revision": "271fffeeb07c9f3c13bd",
+    "url": "/substance/static/js/runtime-main.9748ad86.js"
+  }
+]);
